4d10cae0fbf52e2cdadc4746d5d463c9695e5b01
Super-T02 <Super-T02@users.noreply.github.com>
2021-06-08 16:54:43 +0200

    added DB connection

--- include/head.php
+++ include/head.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
     <link rel="stylesheet" href="include/style/mainStyle.css">
-    <title>Forum für dumme Frage - <?php echo $title?></title>
+    <title>Forum für dumme Fragen - <?=$title?></title>
 
 </head>
 <body>
